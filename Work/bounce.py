ea3aaf611bf0a6484195f490d5df6345992badc1
Emilio Araos <ea3dev@gmail.com>
2020-06-20 15:27:01 -0600

    tutorial

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,10 @@
 # bounce.py
 #
 # Exercise 1.5
+
+height = 100        # In meters
+bounceBack = 0.6    # In meters
+
+for i in range(1, 11):
+    height = height * bounceBack
+    print(i, round(height, 4))
