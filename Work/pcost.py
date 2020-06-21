7b1fd143d0dbb2713332bc82c73fc9958272f6e9
Emilio Araos <ea3dev@gmail.com>
2020-06-21 01:46:29 -0600

    csv files

--- Work/pcost.py
+++ Work/pcost.py
@@ -13,11 +13,15 @@
 # Exercise 1.27
 
 
-with open('Data/portfolio.csv', 'rt') as file:
-    header = next(file)
-    total = 0
-    for line in file:
-        line2 = line.split(',')
-        total += (int(line2[1])) * (float(line2[2]))
-    print(total)
+def portfolio_cost(filename):
+    with open(filename, 'rt') as file:
+        header = next(file)
+        total = 0
+        for line in file:
+            line2 = line.split(',')
+            total += (int(line2[1])) * (float(line2[2]))
+        return total
 
+
+cost = portfolio_cost('Data/portfolio.csv')
+print(cost)
