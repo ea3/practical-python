d525649a72880fad291cf0c0e204a5c20ab23d8c
Emilio Araos <ea3dev@gmail.com>
2020-07-22 12:45:52 -0600

    try-except stock options

--- Work/report.py
+++ Work/report.py
@@ -1,9 +1,5 @@
-# report.py
-#
-# Exercise 2.4
 import csv
-
-portfolio = []
+from pprint import pprint
 
 
 # def read_portfolio(filename):
@@ -24,23 +20,47 @@ portfolio = []
 # print(cost)
 
 # Now as a dictionary:
+
+
 def read_portfolio(filename):
 
     """ Computes the total cost (shares*price) of a portfolio file """
 
     with open(filename, 'rt') as file:
+
+        portfolio = []
         header = next(file)
         total = 0
         for line in file:
             line2 = line.split(',')
-            total += (int(line2[1])) * (float(line2[2]))
-        return total
+            # total += (int(line2[1])) * (float(line2[2]))
+            # print((int(line2[1])))
+            # print((float(line2[2])))
+            dictionary_stocks = {'name': line2[0],
+                                 'shares': int(line2[1]),
+                                 'price': float(line2[2].rstrip())}
+            portfolio.append(dictionary_stocks)
+        return portfolio
+
+
+def read_prices(filename):
+
+    prices = {}
+    with open(filename) as f:
+        rows = csv.reader(f)
+        for row in rows:
+            try:
+                prices[row[0]] = float(row[1])
+            except IndexError:
+                pass
+    return prices
 
 
 cost = read_portfolio('Data/portfolio.csv')
-print(cost)
-
-
+stock_prices = read_prices('Data/prices.csv')
+pprint(cost)
+print('***************************')
+pprint(stock_prices)
 
 
 
