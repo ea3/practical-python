955c0d817a786a055639a813982b98d25919db60
Emilio Araos <ea3dev@gmail.com>
2020-07-22 10:08:19 -0600

    report.py update

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,49 @@
 # report.py
 #
 # Exercise 2.4
+import csv
+
+portfolio = []
+
+
+# def read_portfolio(filename):
+#
+#     """ Computes the total cost (shares*price) of a portfolio file """
+#
+#     with open(filename, 'rt') as rows:
+#         header = next(rows)
+#         print(header)
+#         for row in rows:
+#             line = row.split(',')
+#             holding = (line[0], int(line[1]), float(line[2]))
+#             portfolio.append(holding)
+#         return portfolio
+#
+#
+# cost = read_portfolio('Data/portfolio.csv')
+# print(cost)
+
+# Now as a dictionary:
+def read_portfolio(filename):
+
+    """ Computes the total cost (shares*price) of a portfolio file """
+
+    with open(filename, 'rt') as file:
+        header = next(file)
+        total = 0
+        for line in file:
+            line2 = line.split(',')
+            total += (int(line2[1])) * (float(line2[2]))
+        return total
+
+
+cost = read_portfolio('Data/portfolio.csv')
+print(cost)
+
+
+
+
+
+
+
+
