6ac25a2023cef0ee399da3c9ce60df12b7062e7a
Emilio Araos <ea3dev@gmail.com>
2020-08-11 00:29:11 -0600

    generators

--- Work/stock.py
+++ Work/stock.py
@@ -1,12 +1,25 @@
 class Stock:
+    # __slots__ = ('name', '_shares', 'price')
+
     def __init__(self, name, shares, price):
         self.name = name
         self.shares = shares
         self.price = price
 
+    @property
     def cost(self):
         total_cost = self.shares * self.price
         return total_cost
+
+    @property
+    def shares(self):
+        return self._shares
+
+    @shares.setter
+    def shares(self, value):
+        if not isinstance(value, int):
+            raise TypeError('Expected int')
+        self._shares = value
 
     def sell(self, shares_to_sell):
         self.shares = self.shares - shares_to_sell
